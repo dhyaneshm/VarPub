640fbd9c2b8abecf43d161374ad99588ee3a7707
Khalid Mahmood <mahmood.khalid@gmail.com>
2015-05-12 09:56:45 +1000

    Add usage example

--- src/readvcf.py
+++ src/readvcf.py
@@ -1,9 +1,39 @@
-import pybedtools
+#!/usr/bin/python
+
+import sys
+import getopt
 import os
+import pybedtools
+
+
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print 'readvcf.py -i <inputfile> -o <outputfile>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'readvcf.py -i <inputfile> -o <outputfile>'
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+    print 'Input VCF file is "', inputfile
+    print 'Output file is "', outputfile
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
 
 cpath = os.path.dirname(os.path.abspath(__file__))
 
+#kgbed = pybedtools.BedTool('/Users/khalidm/Development/repositories/VarPub/data/1kg.phase1.snp.bed.gz')
 kgbed = pybedtools.BedTool('/Users/khalidm/Development/repositories/VarPub/data/1kg.phase1.snp.bed.gz')
+#avcf = pybedtools.BedTool('/Users/khalidm/Development/repositories/VarPub/data/BRCA2.snpeff.vcf')
 avcf = pybedtools.BedTool('/Users/khalidm/Development/repositories/VarPub/data/BRCA2.snpeff.vcf')
 
 # print "cat a.bed\n" + str(avcf)
