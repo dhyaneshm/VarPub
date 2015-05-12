7fffd2068aa366da299f19334cbe182bb49c7dc9
Khalid Mahmood <mahmood.khalid@gmail.com>
2015-05-12 12:46:37 +1000

    Improve usage information

--- src/readvcf.py
+++ src/readvcf.py
@@ -1,48 +1,43 @@
 #!/usr/bin/python
 
 import sys
-import getopt
 import os
+import argparse
+import getopt
 import pybedtools
 
+#class Error(Exception):
+#    """Base-class for exceptions in this module."""
+
+#class UsageError(Error):
+#    def __init__(self, msg):
+#        self.msg = msg
 
 def main(argv):
     inputfile = ''
     outputfile = ''
-    try:
-        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-    except getopt.GetoptError:
-        print 'readvcf.py -i <inputfile> -o <outputfile>'
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print 'readvcf.py -i <inputfile> -o <outputfile>'
-            sys.exit()
-        elif opt in ("-i", "--ifile"):
-            inputfile = arg
-        elif opt in ("-o", "--ofile"):
-            outputfile = arg
-    print 'Input VCF file is "', inputfile
-    print 'Output file is "', outputfile
+    vcf = ''
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--vcf", type=str, dest="vcf", help="Input variant file (vcf)", required=True)
+    parser.add_argument("-o", "--output", type=str, dest="out", help="Output file (tabular)", required=True)
+    parser.add_argument("-v", "--verbosity", action="count", default=0)
+
+    args = parser.parse_args()
+
+    #answer = args.x**args.y
+    #print "INPUT = " , args.vcf
+
+    if args.verbosity >= 2:
+        print "{} to the power {} equals {}".format(args.v, args.o, answer)
+    elif args.verbosity >= 1:
+        print "{}^{} == {}".format(args.x, args.y, answer)
+    else:
+        print "Starting ..."
+
+    avcf = pybedtools.BedTool(args.vcf)
+    print avcf[0][7] + " -> " + avcf[0][8]
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
-
-
-cpath = os.path.dirname(os.path.abspath(__file__))
-
-#kgbed = pybedtools.BedTool('/Users/khalidm/Development/repositories/VarPub/data/1kg.phase1.snp.bed.gz')
-kgbed = pybedtools.BedTool('/Users/khalidm/Development/repositories/VarPub/data/1kg.phase1.snp.bed.gz')
-#avcf = pybedtools.BedTool('/Users/khalidm/Development/repositories/VarPub/data/BRCA2.snpeff.vcf')
-avcf = pybedtools.BedTool('/Users/khalidm/Development/repositories/VarPub/data/BRCA2.snpeff.vcf')
-
-# print "cat a.bed\n" + str(avcf)
-# print "cat b.bed\n" + str(bfile)
-
-# print intersect
-# print avcf.intersect(kgbed)
-# print length
-# print len(a)
-
-print avcf[0][7] + " -> " + avcf[0][8]
+    main(sys.argv[1:])
 
