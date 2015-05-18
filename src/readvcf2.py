d8d721572a418afe642c16dbb7ebd8fd223b266b
Khalid Mahmood <mahmood.khalid@gmail.com>
2015-05-18 17:19:17 +1000

    Using pyvcf for reading type 2

---
+++ src/readvcf2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import sys
+import os
+import argparse
+import getopt
+#import pybedtools
+import vcf
+import array
+
+#class Error(Exception):
+#    """Base-class for exceptions in this module."""
+
+#class UsageError(Error):
+#    def __init__(self, msg):
+#        self.msg = msg
+
+def main(argv):
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--vcf", type=str, dest="vcf", help="Input variant file (vcf)", required=True)
+    parser.add_argument("-o", "--output", type=str, dest="out", help="Output file (tabular)", required=True)
+    parser.add_argument("-v", "--verbosity", action="count", default=0)
+
+    args = parser.parse_args()
+
+    if args.verbosity >= 2:
+        print "{} to the power {} equals {}".format(args.v, args.o, answer)
+    elif args.verbosity >= 1:
+        print "{}^{} == {}".format(args.x, args.y, answer)
+    #else:
+    #    print "Starting ..."
+
+    vcf_reader = vcf.Reader(open(args.vcf, 'r'))
+    for record in vcf_reader:
+        print record, record.INFO['AF']
+
+
+if __name__ == "__main__":
+    main(sys.argv)
+
