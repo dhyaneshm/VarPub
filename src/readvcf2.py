0f66bacc798d3cc39659ead7e044d5111050b9bd
Khalid Mahmood <mahmood.khalid@gmail.com>
2015-05-19 16:59:43 +1000

    Fix when csq not present

--- src/readvcf2.py
+++ src/readvcf2.py
@@ -32,16 +32,37 @@ def main(argv):
     #else:
     #    print "Starting ..."
 
+    outputfile.write("chr\tpos\tref\talt\tannotation\tgene_name\tlof\texon\taa_pos\tpoly/sift\teur_maf")
+
+
     vcf_reader = vcf.Reader(open(args.vcf, 'r'))
     for record in vcf_reader:
         current_chr = record.CHROM
         current_pos = record.POS
         current_ref = record.REF
-        print record.ALT
-        current_alt = str(record.ALT)
-        #print record, record.INFO['Het_FIN']
+        current_alt = ','.join(str(v) for v in record.ALT)
 
-        out_str = ["chr"+current_chr, current_pos, current_ref, current_alt]
+        # VEP
+        if "CSQ" in record.INFO:
+            current_vep = record.INFO['CSQ'][0].split('|')
+            current_feature = current_vep[2]
+            current_feature_type = current_vep[3]
+            current_consequence = current_vep[4]
+        else:
+            current_feature = ''
+            current_feature_type = ''
+            current_consequence = ''
+
+
+        # SnpEff
+        current_ann = record.INFO['ANN'][0].split('|')
+        current_annotation = current_ann[1]
+        # current_annotation_impact = current_ann[2]
+        current_gene = current_ann[3]
+
+
+        out_str = ["chr"+current_chr, str(current_pos), current_ref, current_alt,
+                current_annotation, current_gene ]
         out_str = [x or '.' for x in out_str]
         outputfile.write("\t".join(out_str))
         outputfile.write("\n")
