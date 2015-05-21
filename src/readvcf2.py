db2166cbffd8c8bd3e73936c83f3c4e38413b4ef
Khalid Mahmood <mahmood.khalid@gmail.com>
2015-05-21 14:20:12 +1000

    Bug with wrong cadd file name

--- src/readvcf2.py
+++ src/readvcf2.py
@@ -82,7 +82,7 @@ def main(argv):
         current_aa_pos = ann[13]
 
         #CADD
-        for row in tbx.fetch("chr"+current_chr, current_pos, current_pos):
+        for row in cadd_tbx.fetch("chr"+current_chr, current_pos, current_pos):
             row_info = row.split("\t")
             cadd_ref = row_info[2]
             cadd_alt = row_info[4]
